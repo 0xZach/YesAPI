713d16f1e62a472003ac9bd2fd71249d21e17096
0xZach <sylvainmasclet24@gmail.com>
2024-07-08 13:55:02 +0300

    Cleaned Url files in order to clearly define the use of each file.

--- yes/urls.py
+++ yes/urls.py
@@ -1,46 +1,9 @@
-from django.urls import path, include
-from django.contrib.auth.models import Group,  User
-from rest_framework import routers, serializers, viewsets
+from rest_framework.routers import DefaultRouter
+from yes.views import PeopleViewSet,CountriesViewSet
 
-#from . import views
+# The router method helps define the different urls that are gonna be used
+router = DefaultRouter()
+router.register(r'People', PeopleViewSet, basename="people")
+router.register(r'Countries', CountriesViewSet, basename="countries")
 
-
-# Serializers define the API representation.
-class UserSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = User
-        fields = ['url', 'username', 'email', 'groups']
-
-
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Group
-        fields = ['url', 'name']
-
-
-# ViewSets define the view behavior.
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-# Routers provide an easy way of automatically determining the URL conf.
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
-
-
-# urls inside the app
-urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-]
-
-
-""" EXAMPLES
-    # when using a class, need to call the as_view method as we don't use a simple view function
-    path("", views.Index, name="index"), 
-    path("ascent_details/<int:pk>/", views.AscentDetailsView.as_view(), name="ascent-details"),
-    path("<int:pk>/delete/",views.AscentDeleteView.as_view(), name="ascent-delete"),
-    path("list/", views.ascent_list, name="list"),
-    path("upload/", views.upload_ascent, name="upload-ascent"),
-
-"""
+urlpatterns = router.urls
