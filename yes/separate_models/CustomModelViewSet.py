368646eba33dafa77f6a5a9e05cfa55eb15a5550
0xZach <sylvainmasclet24@gmail.com>
2024-07-30 13:58:23 +0300

    switched up so that the commented function is not the first thing we see in the CustomModelViewSet

--- yes/separate_models/CustomModelViewSet.py
+++ yes/separate_models/CustomModelViewSet.py
@@ -7,11 +7,6 @@ Making a custom model viewset as to not repeat everytime I need a method
 that will anyway be repeated in most cases.
 """
 class CustomModelViewSet(ModelViewSet):
-
-    # PATCH is used by default for updating but in case i need it later, 
-    # here is the default for PUT
-    #def put(self, request, *args, **kwargs):
-    #    return self.update(request, *args, **kwargs)
     
     # DRF comes equiped with base keywords for permissions and one can
     # arrange this simple method to setup permissions as they please
@@ -21,3 +16,9 @@ class CustomModelViewSet(ModelViewSet):
         else:
             permission_classes = [IsAdminUser]
         return [permission() for permission in permission_classes]
+    
+
+    # PATCH is used by default for updating but in case i need it later, 
+    # here is the default for PUT
+    #def put(self, request, *args, **kwargs):
+    #    return self.update(request, *args, **kwargs)
